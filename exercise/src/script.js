b1076124c99e6c36f1b6bd72dfdac7d2e6141d29
sidharth-n <sidharthnofficial@gmail.com>
2023-03-01 12:01:23 +0530

    vite setup done with sample code

---
+++ exercise/src/script.js
@@ -0,0 +1,24 @@
+import * as THREE from "three";
+console.log(THREE);
+
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+const sizes = {
+  height: 600,
+  width: 800,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 0;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector(".webgl"),
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
