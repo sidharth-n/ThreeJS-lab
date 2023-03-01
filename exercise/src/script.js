bd8097ebb8aedee98dd3f0e0b4657f6ca5f26082
sidharth-n <sidharthnofficial@gmail.com>
2023-03-01 14:39:46 +0530

    camera rotation based on mouse move

--- exercise/src/script.js
+++ exercise/src/script.js
@@ -1,5 +1,13 @@
 import * as THREE from "three";
-console.log(THREE);
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
 
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -11,9 +19,14 @@ const sizes = {
   width: window.innerWidth,
 };
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 3;
-camera.position.x = 0;
+
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -23,9 +36,12 @@ renderer.setSize(sizes.width, sizes.height);
 const clock = new THREE.Clock();
 
 const rotate = () => {
-  const elapsedTime = clock.getElapsedTime();
-  console.log(elapsedTime);
-  cube.rotation.y = elapsedTime * 2;
+  /*  const elapsedTime = clock.getElapsedTime();
+  cube.rotation.y = elapsedTime * 2; */
+
+  camera.position.x = -cursor.x * 10;
+  camera.position.y = cursor.y * 10;
+  camera.lookAt(cube.position);
   renderer.render(scene, camera);
   requestAnimationFrame(rotate);
 };
