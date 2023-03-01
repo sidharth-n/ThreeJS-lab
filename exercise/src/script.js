907b1e1f861c8bc275882468f3ef1886a6a3132c
sidharth-n <sidharthnofficial@gmail.com>
2023-03-01 15:19:26 +0530

    added orbital controls

--- exercise/src/script.js
+++ exercise/src/script.js
@@ -1,36 +1,39 @@
 import * as THREE from "three";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+const canvas = document.querySelector(".webgl");
 const cursor = {
   x: 0,
   y: 0,
 };
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = event.clientY / sizes.height - 0.5;
-});
-
-const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 const sizes = {
   height: window.innerHeight,
   width: window.innerWidth,
 };
-
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
   100
 );
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
 camera.position.z = 3;
 
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector(".webgl"),
+  canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 const clock = new THREE.Clock();
@@ -39,9 +42,10 @@ const rotate = () => {
   /*  const elapsedTime = clock.getElapsedTime();
   cube.rotation.y = elapsedTime * 2; */
 
-  camera.position.x = -cursor.x * 10;
+  /*   camera.position.x = -cursor.x * 10;
   camera.position.y = cursor.y * 10;
-  camera.lookAt(cube.position);
+  camera.lookAt(cube.position); */
+  controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(rotate);
 };
