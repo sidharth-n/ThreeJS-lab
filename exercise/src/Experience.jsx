62fdfa33d8b6ad849b5efe8ab95f6e774a941bf0
sidharth-n <sidharthnofficial@gmail.com>
2023-05-26 13:57:39 +0530

    pure stage with all options

--- exercise/src/Experience.jsx
+++ exercise/src/Experience.jsx
@@ -21,8 +21,23 @@ import music from "./bgm.mp3";
 export default function Experience() {
   const model = useGLTF("./iphone2.gltf");
 
-  const { animationName = "" } = useControls("Animation", {
-    animationName: { options: ["000", "Freeze", "Flair"] },
+  const { preset, environment, shadow } = useControls({
+    preset: { options: ["rembrandt", "soft", "upfront", "portrait"] },
+    environment: {
+      options: [
+        "sunset",
+        "dawn",
+        "night",
+        "warehouse",
+        "forest",
+        "apartment",
+        "studio",
+        "city",
+        "park",
+        "lobby",
+      ],
+    },
+    shadow: { options: ["contact", "accumulative"] },
   });
 
   const dirLight = useRef();
@@ -35,67 +50,35 @@ export default function Experience() {
 
   return (
     <>
-      {/*   <Environment
-          ground={{
-            height: 7,
-            radius: 100,
-            scale: 100,
-          }}
-          files={"./environmentMaps/studio_small_03_1k.hdr"}
-        /> */}
-      {/*  <Perf position="top-left" /> */}
+      {environment !== "none" && (
+        <Environment
+          preset={environment}
+          files={`./environmentMaps/${environment}`}
+        />
+      )}
 
-      <directionalLight
+      {/* <Perf position="top-left" /> */}
+
+      {/*    <directionalLight
         ref={dirLight}
         castShadow
         position={[1, 2, 3]}
         intensity={1.5}
         shadow-mapSize={[1024, 1024]}
-      />
+      /> */}
 
-      {/*  <Sky sunPosition={sunPosition} /> */}
-      {/*   <ambientLight intensity={0.5} /> */}
-      {/* 
-      <mesh position={[-2, 1, 0]} castShadow>
-        <sphereGeometry />
-        <meshStandardMaterial />
-      </mesh>
-
-      <mesh
-        position-x={2}
-        position-y={0}
-        scale={[1, 1, 1]}
-        ref={cube}
-        castShadow
+      <Stage
+        preset={preset}
+        intensity={1}
+        environment={environment !== "none" && environment}
+        shadows={shadow}
       >
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh> */}
-
-      {/*  <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-        
-      >
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh> */}
-      <Stage>
         <primitive
           object={model.scene}
           scale={[1, 1, 1]}
           rotation={[Math.PI / 2, 0, 0]}
           receiveShadow
         />
-        {/*   <Clone
-        object={model.scene}
-        scale={[0.025, 0.025, 0.025]}
-        receiveShadow
-        position
-        -x={-4}
-      /> */}
         <OrbitControls makeDefault />
       </Stage>
     </>
