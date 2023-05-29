be1766403788667791a2663c16496b0947cd93c8
sidharth-n <sidharthnofficial@gmail.com>
2023-05-29 15:15:15 +0530

    different model loding testing

--- exercise/src/Experience.jsx
+++ exercise/src/Experience.jsx
@@ -18,6 +18,7 @@ import * as THREE from "three";
 import { useControls } from "leva";
 import music from "./bgm.mp3";
 import { Stage } from "././CustomStage/Stage";
+import React from "react";
 
 export default function Experience() {
   const { preset, environment, shadow, extraLight, model } = useControls({
@@ -39,22 +40,26 @@ export default function Experience() {
     shadow: { options: ["contact", "accumulative"] },
     extraLight: { value: false },
     model: {
-      value: "./iphone2.gltf",
-      options: ["./iphone.gltf", "./iphone.glb","./iphone2.gltf"],
+      value: "./laptop.gltf",
+      options: ["./iphone.gltf", "./iphone.glb", "./iphone2.gltf"],
     },
   });
 
   const modelGLTF = useGLTF(model);
-
+  useEffect(() => {
+    console.log("Rotation-x : " + modelGLTF.scene.rotation.x);
+    console.log("Rotation-y : " + modelGLTF.scene.rotation.y);
+    console.log("Rotation-z : " + modelGLTF.scene.rotation.z);
+  }, [model]);
   const spotLight = useRef();
   const dirLight = useRef();
 
   useHelper(dirLight, THREE.DirectionalLightHelper, 1);
-  /*   useFrame((state, delta) => {
+  /*       useFrame((state, delta) => {
     const { camera } = state;
 
     const angle = Date.now() * 0.0009; // Adjust rotation speed as needed
-    const radius = 0.3; // Adjust distance from the object as needed
+    const radius = 4; // Adjust distance from the object as needed
     const objectPosition = new THREE.Vector3(0, 0, 0); // Set the position of the object
 
     const cameraX = objectPosition.x + Math.cos(angle) * radius;
@@ -63,10 +68,13 @@ export default function Experience() {
     camera.position.set(cameraX, cameraY, cameraZ);
 
     camera.lookAt(objectPosition);
-  }); */
+  });  */
+
+  // Add a key to the Stage component to force a re-render when the model changes
+  const stageKey = useMemo(() => model, [model]);
 
   return (
-    <>
+    <React.Fragment key={stageKey}>
       {environment !== "none" && (
         <Environment
           preset={environment}
@@ -89,19 +97,16 @@ export default function Experience() {
       )}
 
       <Stage
+        key={stageKey} // Add the same key to the Stage component
         preset={preset}
         intensity={1}
         environment={environment !== "none" && environment}
         shadows={shadow}
+        adjustCamera={2}
       >
-        <primitive
-          object={modelGLTF.scene}
-          /*   scale={[1, 1, 1]}
-          rotation={[0, 0, 0]} */
-          receiveShadow
-        />
+        <primitive object={modelGLTF.scene} receiveShadow />
         <OrbitControls makeDefault />
       </Stage>
-    </>
+    </React.Fragment>
   );
 }
